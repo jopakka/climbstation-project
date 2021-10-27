2b029bf4e06141b01d27629f6652adde33d5ee22
jopakka <jopakkamies@gmail.com>
2021-10-27 17:19:13 +0300

    testing ci

--- app/src/androidTest/java/fi/climbstationsolutions/climbstation/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/fi/climbstationsolutions/climbstation/ExampleInstrumentedTest.kt
@@ -20,5 +20,6 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("fi.climbstationsolutions.climbstation", appContext.packageName)
+        assertEquals("fi.climbstationsolutions", appContext.packageName)
     }
 }
