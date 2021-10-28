b761700848c350a9f6b5b1766cd55cefce5834fa
oskar wiiala <oskar.wiiala@metropolia.fi>
2021-10-28 12:42:10 +0300

    preliminary RecyclerView and adapter for fragment_statistics complete

--- app/src/main/java/fi/climbstationsolutions/climbstation/StatisticsFragment.kt
+++ app/src/main/java/fi/climbstationsolutions/climbstation/StatisticsFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import fi.climbstationsolutions.climbstation.adapters.StatisticsAdapter
 
 class StatisticsFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +20,15 @@ class StatisticsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_statistics, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val testArray = arrayOf(1, 2, 3, 4, 5, 6, 7)
+
+        val recyclerView: RecyclerView = view.findViewById(R.id.sessionList)
+        recyclerView.layoutManager = LinearLayoutManager(this.context)
+        recyclerView.adapter = StatisticsAdapter(testArray)
     }
 }
