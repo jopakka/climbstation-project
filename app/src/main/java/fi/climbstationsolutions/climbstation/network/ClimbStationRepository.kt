d030e1d0d3b25077e5c69d0daa8d046df7913062
jopakka <jopakkamies@gmail.com>
2021-10-29 00:11:03 +0300

    updated API changed return values and throwable info

--- app/src/main/java/fi/climbstationsolutions/climbstation/network/ClimbStationRepository.kt
+++ app/src/main/java/fi/climbstationsolutions/climbstation/network/ClimbStationRepository.kt
@@ -12,20 +12,18 @@ object ClimbStationRepository {
      * @param climbStationSerialNo Serialnumber of ClimbStation unit
      * @param userID Users ID
      * @param password Users password
-     * @return [String] ClientKey
-     * @throws NoSuchFieldException There is no clientKey
-     * @throws Exception Something else went wrong
+     * @return [String] ClientKey or null
      */
-    suspend fun login(climbStationSerialNo: String, userID: String, password: String): String {
+    suspend fun login(climbStationSerialNo: String, userID: String, password: String): String? {
         return try {
             val req = LoginRequest(climbStationSerialNo, userID, password)
             val response = call.login(req)
             //            Log.d(TAG, "$response")
 
-            response.clientKey ?: throw NoSuchFieldException("No clientKey")
+            response.clientKey
         } catch (e: Exception) {
             Log.e(TAG, "Login error: ${e.localizedMessage}")
-            throw e
+            null
         }
     }
 
@@ -35,8 +33,6 @@ object ClimbStationRepository {
      * @param climbStationSerialNo Serialnumber of ClimbStation unit
      * @param clientKey Client specific key for verifying user
      * @return [Boolean] Did logout success
-     * @throws NoSuchFieldException There is no response
-     * @throws Exception Something else went wrong
      */
     suspend fun logout(climbStationSerialNo: String, clientKey: String): Boolean {
         return try {
@@ -44,11 +40,10 @@ object ClimbStationRepository {
             val response = call.logout(req)
             //            Log.d(TAG, "Logout: $response")
 
-            response.response?.equals("OK")
-                ?: throw NoSuchFieldException("Response is not valid")
+            response.response?.equals("OK") ?: false
         } catch (e: Exception) {
             Log.e(TAG, "Logout error: ${e.localizedMessage}")
-            throw e
+            false
         }
     }
 
@@ -58,20 +53,18 @@ object ClimbStationRepository {
      * @param climbStationSerialNo Serialnumber of ClimbStation unit
      * @param clientKey Client specific key for verifying user
      * @return [InfoResponse] Info about unit
-     * @throws NoSuchFieldException There is no response
-     * @throws Exception Something else went wrong
      */
-    suspend fun deviceInfo(climbStationSerialNo: String, clientKey: String): InfoResponse {
+    suspend fun deviceInfo(climbStationSerialNo: String, clientKey: String): InfoResponse? {
         return try {
             val req = InfoRequest(climbStationSerialNo, clientKey)
             val response = call.deviceInfo(req)
 //            Log.d(TAG, "DeviceInfo: $response")
 
             if (response.response?.equals("OK") == true) response
-            else throw NoSuchFieldException("Response is not valid")
+            else null
         } catch (e: Exception) {
             Log.e(TAG, "DeviceInfo error: ${e.localizedMessage}")
-            throw e
+            null
         }
     }
 
@@ -82,9 +75,6 @@ object ClimbStationRepository {
      * @param clientKey Client specific key for verifying user
      * @param operation "start" or "stop"
      * @return [Boolean] Did operation was successful
-     * @throws IllegalArgumentException Operation is not "start" or "stop"
-     * @throws NoSuchFieldException There is no response
-     * @throws Exception Something else went wrong
      */
     suspend fun operation(
         climbStationSerialNo: String,
@@ -99,10 +89,10 @@ object ClimbStationRepository {
             val response = call.operation(req)
 //            Log.d(TAG, "$response")
 
-            response.response?.equals("OK") ?: throw NoSuchFieldException("Response is not valid")
+            response.response?.equals("OK") ?: false
         } catch (e: Exception) {
             Log.e(TAG, "Operation error: ${e.localizedMessage}")
-            throw e
+            false
         }
     }
 }
