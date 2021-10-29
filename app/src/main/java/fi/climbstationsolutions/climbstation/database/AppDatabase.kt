c52dcc3cebec0cb4931859e5cd4bce04a2e71dd5
jopakka <jopakkamies@gmail.com>
2021-10-29 11:50:50 +0300

    added type converter for database

--- app/src/main/java/fi/climbstationsolutions/climbstation/database/AppDatabase.kt
+++ app/src/main/java/fi/climbstationsolutions/climbstation/database/AppDatabase.kt
@@ -4,8 +4,11 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import fi.climbstationsolutions.climbstation.utils.Converters
 
 @Database(entities = [Session::class, Data::class], version = 1, exportSchema = false)
+@TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun sessionDao(): SessionWithDataDao
 
