acbd4689f97204ece244a0f45fc5bf10e898b6d6
jopakka <jopakkamies@gmail.com>
2021-11-03 17:33:15 +0200

    updated test imports

--- app/src/androidTest/java/fi/climbstationsolutions/climbstation/FirstTimeUITests.kt
+++ app/src/androidTest/java/fi/climbstationsolutions/climbstation/FirstTimeUITests.kt
@@ -8,18 +8,13 @@ import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import fi.climbstationsolutions.climbstation.sharedprefs.PREF_NAME
-import fi.climbstationsolutions.climbstation.sharedprefs.PreferenceHelper
-import fi.climbstationsolutions.climbstation.sharedprefs.PreferenceHelper.get
-import fi.climbstationsolutions.climbstation.sharedprefs.PreferenceHelper.set
 import fi.climbstationsolutions.climbstation.sharedprefs.SERIAL_NO_PREF_NAME
 import fi.climbstationsolutions.climbstation.ui.MainActivity
 import org.junit.After
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
