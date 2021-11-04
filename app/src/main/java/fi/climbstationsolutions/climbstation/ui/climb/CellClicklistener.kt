6b15f1f4f8ff1c175c16dc493f61f9633125a4ea
patep <patrik.polkki@metropolia.fi>
2021-11-04 16:46:43 +0200

    ClimbFragment UI with recyclerview and viewmodel

---
+++ app/src/main/java/fi/climbstationsolutions/climbstation/ui/climb/CellClicklistener.kt
@@ -0,0 +1,5 @@
+package fi.climbstationsolutions.climbstation.ui.climb
+
+interface CellClicklistener {
+    fun onCellClickListener(profile: DifficultyProfile)
+}
--- app/src/main/java/fi/climbstationsolutions/climbstation/ui/climb/ClimbFragment.kt
+++ app/src/main/java/fi/climbstationsolutions/climbstation/ui/climb/ClimbFragment.kt
@@ -1,23 +1,38 @@
 package fi.climbstationsolutions.climbstation.ui.climb
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import fi.climbstationsolutions.climbstation.R
+import fi.climbstationsolutions.climbstation.databinding.FragmentClimbBinding
 
-class ClimbFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+class ClimbFragment : Fragment(R.layout.fragment_climb), CellClicklistener {
+    private lateinit var binding: FragmentClimbBinding
 
-    }
+    private val viewModel: ClimbViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_climb, container, false)
+    ): View {
+        binding = FragmentClimbBinding.inflate(layoutInflater)
+
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.viewModel = viewModel
+
+        binding.difficultyRv.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = DifficultyRecyclerviewAdapter(this@ClimbFragment)
+        }
+
+        return binding.root
+    }
+
+    override fun onCellClickListener(profile: DifficultyProfile) {
+        viewModel.postValue(profile)
     }
 }
