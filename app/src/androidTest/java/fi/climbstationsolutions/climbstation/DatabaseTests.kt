5df42e95ae818044c1fb83f54de24a86badf8415
oskar wiiala <oskar.wiiala@metropolia.fi>
2021-11-12 15:46:22 +0200

    added UtilTests with a calorieCounter and TypeConverter unit tests as well as a settingsDao user weight tests

--- app/src/androidTest/java/fi/climbstationsolutions/climbstation/DatabaseTests.kt
+++ app/src/androidTest/java/fi/climbstationsolutions/climbstation/DatabaseTests.kt
@@ -1,12 +1,10 @@
 package fi.climbstationsolutions.climbstation
 
+import android.util.Log
 import androidx.room.Room
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import fi.climbstationsolutions.climbstation.database.AppDatabase
-import fi.climbstationsolutions.climbstation.database.Data
-import fi.climbstationsolutions.climbstation.database.Session
-import fi.climbstationsolutions.climbstation.database.SessionWithDataDao
+import fi.climbstationsolutions.climbstation.database.*
 import kotlinx.coroutines.runBlocking
 import org.junit.After
 import org.junit.Assert.assertEquals
@@ -19,6 +17,7 @@ import java.util.*
 class DatabaseTests {
     private lateinit var db: AppDatabase
     private lateinit var sessionDao: SessionWithDataDao
+    private lateinit var settingsDao: SettingsDao
     private lateinit var calendar: Calendar
 
     @Before
@@ -26,6 +25,7 @@ class DatabaseTests {
         val context = InstrumentationRegistry.getInstrumentation().targetContext
         db = Room.inMemoryDatabaseBuilder(context, AppDatabase::class.java).build()
         sessionDao = db.sessionDao()
+        settingsDao = db.settingsDao()
         calendar = Calendar.getInstance()
     }
 
@@ -34,6 +34,7 @@ class DatabaseTests {
         db.close()
     }
 
+    // SessionDao tests
     @Test
     fun insertSessionAndGet(): Unit = runBlocking {
         val session = Session(0, "testing db", calendar.time)
@@ -48,7 +49,8 @@ class DatabaseTests {
     fun insertSessionWithData(): Unit = runBlocking {
         val session = Session(0, "Another db test", calendar.time)
         val sId = sessionDao.insertSession(session)
-        val dataList = listOf(Data(0, sId, 100, -21, 340),
+        val dataList = listOf(
+            Data(0, sId, 100, -21, 340),
             Data(0, sId, 80, 0, 430),
             Data(0, sId, 200, 10, 560)
         )
@@ -63,5 +65,30 @@ class DatabaseTests {
         assertEquals(dataList[0].angle, getSession.data[0].angle)
         assertEquals(dataList[1].speed, getSession.data[1].speed)
         assertEquals(dataList[2].totalDistance, getSession.data[2].totalDistance)
+    }
+
+    //SettingsDao tests
+    @Test
+    fun userWeightTests(): Unit = runBlocking {
+        val inputWeight = BodyWeight(1, 90.0f)
+
+        // insert user weight
+        var userId = settingsDao.insertUserBodyWeight(inputWeight)
+
+        Log.d("DatabaseTests.kt", "insertWeightAndGet id: $userId")
+
+        // get user weight
+        var outputWeight = settingsDao.getBodyWeightById(userId)
+        assertEquals(BodyWeight(1, 90.0f), outputWeight)
+
+        // update user weight
+        val newWeight = 62.4f
+        settingsDao.updateUserBodyWeight(newWeight)
+        outputWeight = settingsDao.getBodyWeightById(userId)
+        assertEquals(BodyWeight(1, 62.4f), outputWeight)
+
+        // try to insert after already having a weight set
+        userId = settingsDao.insertUserBodyWeight(inputWeight)
+        assertEquals(1, userId)
     }
 }
