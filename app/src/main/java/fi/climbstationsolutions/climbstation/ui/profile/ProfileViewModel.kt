246d7501b33b8a6b9474a8c5e742a6ac705f9f92
patep <patrik.polkki@metropolia.fi>
2021-11-16 13:24:55 +0200

    ProfileFragment.kt layout design and recyclerview implementation

---
+++ app/src/main/java/fi/climbstationsolutions/climbstation/ui/profile/ProfileViewModel.kt
@@ -0,0 +1,26 @@
+package fi.climbstationsolutions.climbstation.ui.profile
+
+import android.content.Context
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import fi.climbstationsolutions.climbstation.database.AppDatabase
+import fi.climbstationsolutions.climbstation.ui.viewmodels.ClimbFinishedViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class ProfileViewModel(context: Context): ViewModel() {
+    private val database = AppDatabase.get(context)
+    private val sessionDao = database.sessionDao()
+
+    val allSessions = sessionDao.getAllSessionsWithData()
+}
+class ProfileViewModelFactory(private val context: Context) : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ProfileViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return ProfileViewModel(context) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
