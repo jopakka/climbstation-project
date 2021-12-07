6ebafae9fdde8a60c85b7240a8bf77a740f9eee5
jopakka <jopakkamies@gmail.com>
2021-12-07 15:29:15 +0200

    app is registered to open .json files

---
+++ app/src/main/java/fi/climbstationsolutions/climbstation/ui/ShareActivity.kt
@@ -0,0 +1,13 @@
+package fi.climbstationsolutions.climbstation.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import fi.climbstationsolutions.climbstation.R
+
+class ShareActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_share)
+
+    }
+}
