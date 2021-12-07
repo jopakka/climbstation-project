ada52dc1035bcc1f622884afbf44c1b3540aff4c
jopakka <jopakkamies@gmail.com>
2021-12-07 17:21:07 +0200

    read data from json

--- app/src/main/java/fi/climbstationsolutions/climbstation/ui/ShareActivity.kt
+++ app/src/main/java/fi/climbstationsolutions/climbstation/ui/ShareActivity.kt
@@ -1,13 +1,23 @@
 package fi.climbstationsolutions.climbstation.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import fi.climbstationsolutions.climbstation.R
+import fi.climbstationsolutions.climbstation.utils.ProfileSharer
 
 class ShareActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_share)
 
+        readData()
+    }
+
+    private fun readData() {
+        val path = intent?.data ?: return
+        val sharer = ProfileSharer(this)
+        val test = sharer.getSharedProfile(path)
+        Log.d("TEST", "${test?.profile}")
     }
 }
--- app/src/main/java/fi/climbstationsolutions/climbstation/utils/ProfileSharer.kt
+++ app/src/main/java/fi/climbstationsolutions/climbstation/utils/ProfileSharer.kt
@@ -2,11 +2,13 @@ package fi.climbstationsolutions.climbstation.utils
 
 import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import androidx.core.content.FileProvider
 import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import fi.climbstationsolutions.climbstation.database.ClimbProfileWithSteps
-import java.io.File
-import java.io.FileOutputStream
+import fi.climbstationsolutions.climbstation.network.profile.Profile
+import java.io.*
 
 class ProfileSharer(private val activity: Activity) {
     private val gson = Gson()
@@ -30,6 +32,13 @@ class ProfileSharer(private val activity: Activity) {
         activity.startActivity(sendIntent)
     }
 
+    fun getSharedProfile(uri: Uri): ClimbProfileWithSteps? {
+        val data = readFromFile(uri)
+        return try {
+            Gson().fromJson(data, ClimbProfileWithSteps::class.java)
+        } catch (e: Exception) { null }
+    }
+
     private fun createFile(name: String): File {
         val outputDir = activity.cacheDir
         return File.createTempFile(name, ".json", outputDir)
@@ -40,5 +49,21 @@ class ProfileSharer(private val activity: Activity) {
         stream.use {
             it.write(data.toByteArray())
         }
+    }
+
+    private fun readFromFile(uri: Uri): String {
+        val stream = activity.contentResolver.openInputStream(uri)
+        val writer = StringWriter()
+        val buffer = CharArray(1024)
+
+        stream.use { s ->
+            val reader = BufferedReader(InputStreamReader(s, "UTF-8"))
+            var n: Int
+            while (reader.read(buffer).also { n = it } != -1) {
+                writer.write(buffer, 0, n)
+            }
+        }
+
+        return writer.toString()
     }
 }
